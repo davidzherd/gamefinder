bcb47ab89b90331ed1de14e8440ee1b10fa4f393
davidzherd <155192463+davidzherd@users.noreply.github.com>
2024-03-31 17:47:41 +0300

    rework

--- src/components/FilterBar/FilterBar.jsx
+++ src/components/FilterBar/FilterBar.jsx
@@ -1,69 +1,164 @@
-import React, { useEffect, useState } from 'react'
-import './FilterBar.css'
-import { config } from '../../Assets/config.js'
-import Tooltip from '../Tooltip/Tooltip.jsx';
-import Button from '../Button/Button.jsx';
+import React, { useEffect, useState } from "react";
+import "./FilterBar.css";
+import { config } from "../../Assets/config.js";
+import Button from "../Button/Button.jsx";
 
-export function MobileFilters({ currentPlatforms, currentGaneres, filterAction, closeModal },...props) {
+export function MobileFilters(
+  { currentPlatforms, currentGaneres, filterAction, closeModal },
+  ...props
+) {
   return (
-    <div className='mobile-filter-wrap'>
-      <header className='header-with-exit'>
+    <div className="mobile-filter-wrap">
+      <header className="header-with-exit">
         <h2>Select Filters</h2>
-        <button className='exit' onClick={closeModal}>X</button>
+        <button className="exit" onClick={closeModal}>
+          X
+        </button>
       </header>
-      <main className='mobile-filters'>
-        <div className=' ml-1'>
+      <main className="mobile-filters">
+        <div className=" ml-1">
           <h2>Platforms</h2>
-          <div>{config.platforms.map((platform=><button className={currentPlatforms.includes(platform.name) ? 'ganre-btn selected-filter' : 'ganre-btn'} onClick={()=>filterAction(platform.name, "platform")} key={platform.name}>{platform.name}</button>))}</div>
+          <div>
+            {config.platforms.map((platform) => (
+              <button
+                className={
+                  currentPlatforms.includes(platform.id)
+                    ? "ganre-btn selected-filter"
+                    : "ganre-btn"
+                }
+                onClick={() => filterAction(platform.id, "platform")}
+                key={platform.name}
+              >
+                {platform.name}
+              </button>
+            ))}
+          </div>
         </div>
-        <div className=' ml-1'>
+        <div className=" ml-1">
           <h2>Ganres</h2>
-          <div>{config.ganres.map((ganre=><button className={currentGaneres.includes(ganre.name) ? 'ganre-btn selected-filter' : 'ganre-btn'} onClick={()=>filterAction(ganre.name, "ganre")} key={ganre.name}>{ganre.name}</button>))}</div>
+          <div>
+            {config.ganres.map((ganre) => (
+              <button
+                className={
+                  currentGaneres.includes(ganre.name)
+                    ? "ganre-btn selected-filter"
+                    : "ganre-btn"
+                }
+                onClick={() => filterAction(ganre.name, "ganre")}
+                key={ganre.name}
+              >
+                {ganre.name}
+              </button>
+            ))}
+          </div>
         </div>
       </main>
     </div>
-  )
+  );
 }
 
-
-function FilterBar({ currentPlatforms, currentGaneres, filterAction },...props) {
+function FilterBar(
+  { currentPlatforms, currentGaneres, filterAction },
+  ...props
+) {
   const [modalOpen, setModalOpen] = useState(false);
   const allFilters = [...currentGaneres, ...currentPlatforms];
-  const allFiltersObjects = [...config.ganres,...config.platforms];
+  const allFiltersObjects = [...config.ganres, ...config.platforms];
   useEffect(() => {
     const handleResize = () => {
       setComponentType(window.innerWidth < 900 ? false : true);
     };
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     handleResize();
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
-  const [componentType, setComponentType] = useState(true)
-  const desktop = 
-  <menu className='filter-menu pl-0'>
-    <span className='filter-type center gap-05'>
+  const [componentType, setComponentType] = useState(true);
+  const desktop = (
+    <menu className="filter-menu pl-0">
+      <span className="filter-type center gap-05">
         <h2>Platforms</h2>
-        {config.platforms.map((platform=><div title={platform.name} key={platform.id} className={currentPlatforms.includes(platform.name) ? "has-tootip selected-filter" : "has-tootip"} onClick={()=>filterAction(platform.name, "platform")}><Tooltip tip={platform.name}/><img className='platformImage' key={platform.id} src={platform.icon} alt={platform.name}/></div>))}
-    </span>
-    <span className='filter-type center gap-05'>
-      <h2>Ganres</h2>
-      {config.ganres.map((ganre=><div title={ganre.name} key={ganre.id} className={currentGaneres.includes(ganre.name) ? "has-tootip selected-filter" : "has-tootip"} onClick={()=>filterAction(ganre.name, "ganre")}><Tooltip tip={ganre.name}/><img className='platformImage' key={ganre.id} src={ganre.icon} alt={ganre.name}/></div>))}
-    </span>
-  </menu>;
-  const mobile = <div className='margin-1rem mobile-layout-column'>
-    <Button style="fill-btn" action={()=>setModalOpen(true)}>Apply Filters</Button>
-    {allFilters.length > 0 ? <h2>Applied filters</h2> : <h2>You didn't apply any filters!</h2>}
-    <div className='filter-type center gap-05 align-center mobile-layout-row'>
-      {allFilters.map(((filter)=><img className='platformImage' key={filter} src={allFiltersObjects.filter(f=>f.name === filter)[0].icon} alt={filter}/>))}
+        {config.platforms.map((platform) => (
+          <div
+            title={platform.name}
+            key={"div" + platform.id}
+            className={
+              currentPlatforms.includes(platform.id)
+                ? "has-tootip selected-filter"
+                : "has-tootip"
+            }
+            onClick={() => filterAction(platform.id, "platform")}
+          >
+            <img
+              className="platformImage"
+              key={"tt" + platform.id}
+              src={platform.icon}
+              alt={platform.name}
+            />
+          </div>
+        ))}
+      </span>
+      <span className="filter-type center gap-05">
+        <h2>Ganres</h2>
+        {config.ganres.map((ganre) => (
+          <div
+            title={ganre.name}
+            key={ganre.id}
+            className={
+              currentGaneres.includes(ganre.name)
+                ? "has-tootip selected-filter"
+                : "has-tootip"
+            }
+            onClick={() => filterAction(ganre.name, "ganre")}
+          >
+            <img
+              className="platformImage"
+              key={ganre.id}
+              src={ganre.icon}
+              alt={ganre.name}
+            />
+          </div>
+        ))}
+      </span>
+    </menu>
+  );
+  const mobile = (
+    <div className="margin-1rem mobile-layout-column">
+      <Button style="fill-btn" action={() => setModalOpen(true)}>
+        Apply Filters
+      </Button>
+      {allFilters.length > 0 ? (
+        <h2>Applied filters</h2>
+      ) : (
+        <h2>You didn't apply any filters!</h2>
+      )}
+      <div className="filter-type center gap-05 align-center mobile-layout-row">
+        {allFilters.map((filter) => (
+          <img
+            className="platformImage"
+            key={filter}
+            src={
+              allFiltersObjects.filter(
+                (f) => f.id === filter || f.name === filter
+              )[0].icon
+            }
+            alt={filter}
+          />
+        ))}
+      </div>
+      {modalOpen ? (
+        <MobileFilters
+          currentPlatforms={currentPlatforms}
+          currentGaneres={currentGaneres}
+          filterAction={filterAction}
+          closeModal={() => setModalOpen(false)}
+        />
+      ) : null}
     </div>
-    {modalOpen ? <MobileFilters currentPlatforms={currentPlatforms} currentGaneres={currentGaneres} filterAction={filterAction} closeModal={()=>setModalOpen(false)}/> : null}
-    </div>
+  );
 
-  return (
-    componentType ? desktop : mobile
-  )
+  return componentType ? desktop : mobile;
 }
 
-export default FilterBar
+export default FilterBar;
