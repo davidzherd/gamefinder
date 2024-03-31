dc6ce5a53ad0edd9494e8f164d047b42b5b70413
davidzherd <155192463+davidzherd@users.noreply.github.com>
2024-03-31 17:48:56 +0300

    more responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,48 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import controllerImg from '../../Assets/controller.png'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./Navbar.css";
+import controllerImg from "../../Assets/controller.png";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-    const pages = [{name:'Home', link:"/"}, {name:'Find a game', link:"/findgame"}, {name:'About Us', link:"/about"}];
-    const [page,setPage] = useState('Home')
-    function handlePageSelection(pageName){
-        setPage(pageName);
-    }
+  const pages = [
+    { name: "Home", link: "/" },
+    { name: "Find a game", link: "/findgame" },
+    { name: "About Us", link: "/about" },
+  ];
+  const [page, setPage] = useState("Home");
+  function handlePageSelection(pageName) {
+    setPage(pageName);
+  }
   return (
     <nav>
-      <Link className='remove-link' to="/">
-        <div className='logo' onClick={()=>handlePageSelection('Home')}>
-        <h1>Game<span className='text-color'>Finder</span></h1>
-        <img width="64" height="64" src={controllerImg} alt="controller"/>
+      <Link className="remove-link" to="/">
+        <div className="logo" onClick={() => handlePageSelection("Home")}>
+          <h1>
+            Game<span className="text-color">Finder</span>
+          </h1>
+          <img
+            width="64"
+            height="64"
+            src={controllerImg}
+            alt="controller"
+            className="logo-img"
+          />
         </div>
       </Link>
-        <ul>
-          {pages.map(item=><Link className='remove-link' key={item.name} to={item.link}><li className={item.link===window.location.pathname ? 'selected': null} onClick={()=>handlePageSelection(item.name)}>{item.name}</li></Link>)}
-        </ul>
-             
-        
+      <ul>
+        {pages.map((item) => (
+          <Link className="remove-link" key={item.name} to={item.link}>
+            <li
+              className={item.name === page ? "selected" : null}
+              onClick={() => handlePageSelection(item.name)}
+            >
+              {item.name}
+            </li>
+          </Link>
+        ))}
+      </ul>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
